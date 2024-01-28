02a4ce9ba6ca9a7bb63ca59fdb330d6f8981d55d
etrickel <erik.trickel@asu.edu>
2024-01-28 09:13:18 -0700

    WiP

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-
 rm -rf /usercode/.vscode 
 mkdir /usercode/.vscode 
 cd /usercode/.vscode 
@@ -9,6 +8,8 @@ git clone https://github.com/etrickel/240config.git
 
 cp 240config/mybashrc.sh . 
 cp 240config/settings.json . 
+
+echo "gcc -Werror -Wall -g main.c -o main.bin " > bash_history
 
 exec bash --rcfile /usercode/.vscode/mybashrc.sh
 
